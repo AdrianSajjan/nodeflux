291a9a911943aa34ff9b581dc76c06d262b19b0a
adriansajjan <adriansajjan2001@gmail.com>
2024-07-29 22:10:08 +0530

    wip - about

--- src/components/typography/masked-heading.tsx
+++ src/components/typography/masked-heading.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 export function MaskedHeading({ className, children, ...props }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) {
   return (
-    <h1 {...props} className={cn("text-center text-6xl tracking-tighter font-medium bg-[radial-gradient(circle_farthest-corner_at_50%_50%,_var(--white),_rgba(255,255,255,0))] bg-clip-text text-transparent", className)}>
+    <h1 {...props} className={cn("text-center text-6xl tracking-tighter font-medium bg-[radial-gradient(circle_farthest-corner_at_50%_50%,_rgba(255,255,255,1),_rgba(255,255,255,0.25))] bg-clip-text text-transparent", className)}>
       {children}
     </h1>
   );
