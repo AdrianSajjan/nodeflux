0490e8776295e2815b3fe56a9f5b52ba0368d73b
adriansajjan <adriansajjan2001@gmail.com>
2024-09-19 12:44:00 +0530

    SEO stuffs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import lottie from "astro-integration-lottie";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false }), lottie()],
+  integrations: [react(), tailwind({ applyBaseStyles: false }), lottie(), sitemap()],
 });
